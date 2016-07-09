238f9815b98eb04a637f8e030baf6a6193e5e6ab
aland_000 <aland_000@BRUNA.home>
2016-07-09 03:08:38 -0300

    Configurações do MYSQL e prototipação da tela PesquisaTitulos.html

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -40,6 +40,12 @@ public class TituloController {
 		 return mv;
 	}
 	
+	@RequestMapping
+	public String pesquisar()
+	{
+		return "PesquisaTitulos";
+	}
+	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulos()
 	{
