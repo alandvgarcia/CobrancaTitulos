c2bcd058c6fa5f04a6ff988a0d2cf8fed1bd0ed7
alanvargas <alanvargas@HouseDroid>
2016-07-31 12:43:02 -0300

    Implementação ajax na Pesquisa de Título ao clicar no botão receber.

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -28,6 +28,38 @@ $(function()
 	
 			$('[rel="tooltip"]').tooltip();
 			$('.currency').maskMoney({decimal: ',', thousands: '.' , allowZero: true});
+			
+			$('.js-atualizar-status').on('click', function(event){
+				
+				event.preventDefault();
+				
+				var botaoReceber = $(event.currentTarget);
+				var urlReceber = botaoReceber.attr('href');
+				
+				var response = $.ajax({
+					url: urlReceber,
+					type: 'PUT'
+				});
+								
+				response.done(function(e){
+					var codigoTitulo = botaoReceber.data('codigo');
+					$('[data-role=' + codigoTitulo + ']').html('<span class="label label-success">'+e+'</span>');					
+					botaoReceber.hide();
+					
+					console.log(codigoTitulo);
+					
+				});
+				
+				
+				response.fail(function(e){
+					console.log(e);
+					alert('Erro recebendo cobrança!')
+				});
+				
+				
+				
+			});
+			
 	
 		})
 		
